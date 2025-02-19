95a04aca41622883fa0b764588b9620185c38fa4
umar-abd <umarabdillah93@gmail.com>
2025-02-20 00:01:54 +0800

    Bug 3

--- src/BuggyStorage.sol
+++ src/BuggyStorage.sol
@@ -4,21 +4,30 @@ pragma solidity ^0.8.0;
 contract BuggyStorage {
     // DO NOT EDIT COMMENTS
     // Task 1: Declare an array as numbers;
+    uint[] private numbers;
     // Task 2: Declare a string as message;
     // Task 3: Declare a mapping of address to balance as balances;
 
     // Bug 1: Array bounds
     function getNumber(uint index) public view returns (uint) {
+        require(index < numbers.length, "Index out of bounds");
         return numbers[index]; 
     }
 
     function addNumber(uint number) public {
         // Task 4: Implement adding numbers to the array
+        numbers.push(number);
     }
 
     // Bug 2: Slice array correctly
     function sliceArray(uint start, uint end) public view returns (uint[] memory) {
-        return numbers[start:end];
+        require(start < end && end <= numbers.length, "Invalid slice range");
+
+        uint[] memory result = new uint[](end - start);
+        for (uint i = start; i < end; i++) {
+            result[i - start] = numbers[i];
+        }
+        return result;
     }
 
     // Bug 3: Concatenate strings correctly
