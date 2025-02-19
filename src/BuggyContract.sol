69a4f067d76723b8cf17b1670648ee2435d6fe6c
umar-abd <umarabdillah93@gmail.com>
2025-02-19 23:53:39 +0800

    Homework Done

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -13,57 +13,56 @@ contract BuggyContract {
     }
 
     // Bug 1
-    () {
+    constructor() {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
     // Bug 4
     function divideCount(uint divisor) public returns (uint) {
+        require(divisor !=0, "Cannot devide by zero");
         count = count / divisor;
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
-        messages = message;
+    function setMessage(string memory newMessage) public {
+        messages = newMessage;
     }
 
     // Bug 6
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+    function sumNumbers(uint n) public pure returns (uint) {
+        return (n * (n + 1)) / 2;
     }
 
     // Bug 7
     // Hint: Think about math operation overflow
     function setCountWithMultiplication(uint x, uint y) public {
-        count = x * y;
+        unchecked {
+            count = x * y;
+        }        
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
     // Bug 9
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
